b8d38ee3f259aac1b206aa5eec61166964675886
yannengfa <940436475@qq.com>
2021-03-27 13:34:28 +0800

    initialization

---
+++ src/main/java/com/study/FamilyServicePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyServicePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyServicePlatformApplication.class, args);
+	}
+
+}
